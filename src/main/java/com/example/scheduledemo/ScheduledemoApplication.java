b5cdee6330c5cf8033836e2c7b5c15b42bbb8c19
rutikagulhane <gulhane.rutika@gmail.com>
2021-07-31 11:16:32 -0400

    my first commit

---
+++ src/main/java/com/example/scheduledemo/ScheduledemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.scheduledemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduledemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScheduledemoApplication.class, args);
+    }
+
+}
